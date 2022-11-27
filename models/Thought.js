accf6eb36345588a125f50067e1916604990ef3a
noah138 <noahschwartz@live.com>
2022-11-27 01:02:56 -0800

    Added user and thought models

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,52 @@
+const moment = require('moment/moment');
 const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema()
+const thoughtSchema = new Schema(
+    {
+    thoughtText: {
+        type: String,
+        required: true,
+        min_length: 1,
+        max_length: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+})
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max_length: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            deafult: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+    }
+)
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+})
+
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,5 +1,47 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, SchemaTypeOptions } = require('mongoose');
 
-const userSchema = new Schema()
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+    },
+    email: {
+        type: String,
+        required: true,
+        uique: true,
+        match: [
+            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            'Please add a valid email address.',
+          ]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+
+    ]},
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema)
 
 module.exports = User;
