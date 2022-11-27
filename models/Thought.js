bfcd35464b78ef7b1d1e2787d2dfcbb256f1be17
noah138 <noahschwartz@live.com>
2022-11-27 00:22:07 -0800

    boilerplate

---
+++ models/Thought.js
@@ -0,0 +1,5 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema()
+
+module.exports = Thought;
---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const { Thought } = require('../../models');
+
+module.exports = router;
